9ed96f2d5cb5f5df060f1b277b0020ade5cfd42b
unknown <abirkhan8536@gmail.com>
2026-05-30 05:57:30 -0700

    Error resolved.

--- src/pages/EmployeeInformation/DoctorFitnessCheck.tsx
+++ src/pages/EmployeeInformation/DoctorFitnessCheck.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import { useForm } from "react-hook-form";
 import toast from "react-hot-toast";
 import {
-  Search,
   CalendarDays,
 } from "lucide-react";
 import type { Employee, PhysicalExaminationSetting } from "../../types/interfaces";
